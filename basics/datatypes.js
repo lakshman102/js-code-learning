72c924660d8fcfa645da9d012e6a01548326d83d
Lakshman Tripathi <158588420+lakshman102@users.noreply.github.com>
2026-05-08 12:55:11 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs
+
+let age=18
+let isLoggedIn=false
+let state=null
+
+
+/* 
+primitive datatype:
+number=2
+bigInt
+string=>""
+boolean=>T/F
+null=> standalone value
+undefined=> 
+symbol=> unique    
+*/
+/*  
+object
+*/
+console.log(typeof(undefined)); //undefined
+
+console.log(typeof(null)); ////null is ka type object
---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+const accountId=1434
+let accountEmail="2003lakshtripathi@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let acccountState;
+
+// accountId=456 //not allowed
+
+accountEmail="hc@ha.com"
+acccountPassword="24345"
+accountCity="Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountCity,acccountState])
