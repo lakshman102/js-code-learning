281b9126c805d50ce1692b27f8d08018331c5089
Lakshman Tripathi <158588420+lakshman102@users.noreply.github.com>
2026-05-08 15:00:06 +0000

    operations

---
+++ basics/conversionOperation.js
@@ -0,0 +1,39 @@
+let score="33abc"
+
+
+let valueInNumber=Number(score);
+/* 
+write console.log as a method
+ console.log(typeof (valueInNumber))
+
+*/
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //NaN special Type
+
+
+/*
+"33"=>33
+"33abc"=>NaN
+true=>1;
+false=>0
+Null=>0
+undefined=>Null
+*/
+
+let isLoggedIn="abc"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*In boolean
+1=>true;
+0=>false;
+""=>false
+"abc"=>true
+*/
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
